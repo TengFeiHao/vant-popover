b46350e1de8a2fc69bc61713b342f637a88fead3
haotengfei <haotengfei@zhidoushi.cn>
2022-01-13 22:38:22 +0800

    feat(tab): demo演示

--- src/plugins/vant.ts
+++ src/plugins/vant.ts
@@ -1,8 +1,8 @@
 /**
  * @des 按需导入，涉及到plugins（比如ElMessage），不建议导入，在页面中使用的时候自己导入
  */
-import { Button, Popover } from 'vant'
+import { Button, Popover, Tab, Tabs } from 'vant'
 
-const components = [Button, Popover]
+const components = [Button, Popover, Tab, Tabs]
 
 export default components
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="home">
-    <van-popover v-model:show="showPopover" overlay>
-      <div>
-        <p>页面放大，或者在ipad上，pc端，会看见三角形下面有个小间隙，</p>
-        <p>我看了你是通过，transform: translate实现的逻辑上没啥问题，</p>
-        <p>但是会有间隙，通过margin 偏移实现出现间隙的概率小点，不知道为啥，是不是有点吹毛求疵了，😄</p>
-        <p>在ipad pro 上间隙比较明显</p>
-      </div>
-      <template #reference>
-        <van-button type="primary">深色风格</van-button>
-      </template>
-    </van-popover>
+    <van-tabs v-model:active="active">
+      <van-tab title="标签 1">{{ des }}</van-tab>
+      <van-tab title="标签 2">{{ des }}</van-tab>
+      <van-tab title="标签 3">{{ des }}</van-tab>
+      <van-tab title="标签 4">{{ des }}</van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -19,10 +14,9 @@ import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   setup() {
-    const showPopover = ref(false)
-    return {
-      showPopover
-    }
+    const active = ref(0)
+    const des = ref('手机端预览，切换tab会发现字体上下跳，我觉得van-tab已经用了flex的center就没必要写line-height了，即使换行也有让字体默认的line-height，去掉line-height的样式就不会上下跳了')
+    return { active, des }
   }
 })
 </script>
